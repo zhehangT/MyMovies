decc4ab4e6e8897ff4afdbcd6ee87a0bf138a7da
t_zhehang <zhehangt@163.com>
2017-05-17 22:06:07 +0800

    remove data

--- Main.py
+++ Main.py
@@ -7,8 +7,8 @@ from MovieManager import MovieManager
 import os
 
 s = os.sep
-# movieRoot = "E:" + s + u"电影" + s
-movieRoot = "E:" + s + u"IMDB" + s
+movieRoot = "F:" + s + u"电影" + s
+# movieRoot = "E:" + s + u"IMDB" + s
 
 
 if __name__ == "__main__":
